bde911a61c83899fea5c3a8d3fe5b6960faf0dc6
itsvihanga <IT21251078@my.sliit.lk>
2023-05-04 12:30:39 +0530

    filr sturecture and all

---
+++ app/src/main/java/com/example/sampalproject01/EmployeeModel.kt
@@ -0,0 +1,11 @@
+package com.example.sampalproject01
+
+data class EmployeeModel(
+    var empId: String? = null,
+    var empName: String? = null,
+    var empDis: String? = null,
+    var empPrice: String? = null
+)
+
+
+
---
+++ app/src/main/java/com/example/sampalproject01/FetchingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sampalproject01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FetchingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fetching)
+    }
+}
